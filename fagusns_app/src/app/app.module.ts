257e898497a38cd0e04154e9abe743407e336a25
vladavoX <vlada.pzs@hotmail.com>
2022-05-21 11:22:36 +0200

    Added Components: Company, Contact, Footer, Recent; small adjustments to code

--- fagusns_app/src/app/app.module.ts
+++ fagusns_app/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './components/landing/landing.component';
 import { AboutComponent } from './components/about/about.component';
 import { WorkComponent } from './components/work/work.component';
+import { RecentComponent } from './components/recent/recent.component';
+import { CompanyComponent } from './components/company/company.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +19,14 @@ import { WorkComponent } from './components/work/work.component';
     NavbarComponent,
     LandingComponent,
     AboutComponent,
-    WorkComponent
+    WorkComponent,
+    RecentComponent,
+    CompanyComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule
   ],
